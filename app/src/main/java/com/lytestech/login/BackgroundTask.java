177fa4f6966760456ecf032ae2393f277f8c0a52
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2017-07-17 19:46:09 +0300

    connected to web

--- app/src/main/java/com/lytestech/login/BackgroundTask.java
+++ app/src/main/java/com/lytestech/login/BackgroundTask.java
@@ -2,12 +2,23 @@ package com.lytestech.login;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by gideon bamuleseyo on 5/11/2017.
  */
 
-public class BackgroundTask extends AsyncTask<String, Void, Void> {
+public class BackgroundTask extends AsyncTask<String, Void, String> {
     Context ctx;
     BackgroundTask(Context ctx){
         this.ctx = ctx;
@@ -18,11 +29,38 @@ public class BackgroundTask extends AsyncTask<String, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(String... params) {
-        String reg_url = "";
-        String login_url = "";
-        String method = "";
-        //if(){}
+    protected String doInBackground(String... params) {
+        String reg_url = "http://192.168.1.14/webapp/register.php";
+        String login_url = "http://10.0.2.2/webapp/login.php";
+        String method = params[0];
+        if(method.equals("register")){
+            String name = params[1];
+            String user_name = params[2];
+            String user_pass = params[3];
+
+            try {
+                URL url = new URL(reg_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data = URLEncoder.encode("user", "UTF-8")+"=" + URLEncoder.encode(name, "UTF-8")+"&"+
+                            URLEncoder.encode("user_name", "UTF-8")+"=" + URLEncoder.encode(user_name, "UTF-8")+"&"+
+                        URLEncoder.encode("user_pass", "UTF-8")+"=" + URLEncoder.encode(user_pass, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getErrorStream();
+                IS.close();
+                return "Registration successful";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return null;
     }
@@ -33,7 +71,7 @@ public class BackgroundTask extends AsyncTask<String, Void, Void> {
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(String result) {
+        Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/lytestech/login/SignUpActivity.java
+++ app/src/main/java/com/lytestech/login/SignUpActivity.java
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+
+import org.json.JSONObject;
 
 /**
  * Created by gideon bamuleseyo on 5/11/2017.
@@ -13,12 +20,14 @@ import android.widget.EditText;
 public class SignUpActivity extends AppCompatActivity {
     EditText etName, etUserName, etPassword;
     Button bRegister;
-    String mName, mUserName, mPassword;
+    String mName, mUserName, mPassword, mEmail;
+    AQuery aq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        aq = new AQuery(this);
         etName = (EditText) findViewById(R.id.edit_txt_name);
         etUserName = (EditText) findViewById(R.id.edit_txt_username);
         etPassword = (EditText) findViewById(R.id.edit_txt_password);
@@ -27,14 +36,25 @@ public class SignUpActivity extends AppCompatActivity {
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mName = etName.getText().toString();
-                mUserName = etUserName.getText().toString();
-                mPassword = etPassword.getText().toString();
-                String method = "register";
-                BackgroundTask backgroundtask = new BackgroundTask(SignUpActivity.this);
-                backgroundtask.execute(method, mName, mUserName, mPassword);
+                register();
             }
         });
 
+    }
+
+    private void register(){
+        mName = etName.getText().toString();
+        mUserName = etUserName.getText().toString();
+        mPassword = etPassword.getText().toString();
+        mEmail = aq.id(R.id.edit_txt_email).getText().toString();
+
+        String url = "http://lytestech.ga/api/lytes/register";
+        url += "?myname=" + mName  + "&email=" + mEmail +  "&username=" + mUserName + "&password=" + mPassword;
+        aq.ajax(url, String.class, new AjaxCallback<String>(){
+            @Override
+            public void callback(String url, String response, AjaxStatus status) {
+                Toast.makeText(SignUpActivity.this, response, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
